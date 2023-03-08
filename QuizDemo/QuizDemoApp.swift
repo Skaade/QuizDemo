7662fa39432dd82f6d9d071b1f3ab504a582a288
max <maxmagi@hotmail.com>
2023-03-08 17:32:44 +0100

    Initial Commit

---
+++ QuizDemo/QuizDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizDemoApp.swift
+//  QuizDemo
+//
+//  Created by dmu mac 25 on 08/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct QuizDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
